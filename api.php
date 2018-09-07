938f909b8600080903f01e63a3310e12d6a349fe
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2018-09-08 07:29:21 +0800

    tambah

---
+++ api.php
@@ -0,0 +1,31 @@
+<?php
+/**  
+ * Contoh API Sederhana
+ * irul @ 20171102
+ */
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case 'GET':
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    echo "$method: " . $data->nama;
+    break;
+    case 'POST':
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    echo "$method: " . $data;
+    break;
+    case 'PUT':
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    echo "$method: " . $data->nama;
+    case 'DELETE':
+    $json = file_get_contents('php://input');
+    $data = json_decode($json);
+    echo "$method: " . $data->nama;
+    break;
+    default:
+    echo "Method not defined!";
+}
+?>
